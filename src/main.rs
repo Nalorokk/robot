753188cc39e60093fc9f262f4401df521ea6ba28
NermaN <nalorokk@gmail.com>
2018-07-01 03:02:43 +0000

    small fixes

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ struct AppState {
     power: Arc<AtomicUsize>,
 }
 
-fn cmd(req: HttpRequest<AppState>) -> impl Responder {
+fn on_cmd(req: HttpRequest<AppState>) -> impl Responder {
     let info = Path::<(String)>::extract(&req).unwrap();
     let cmd = info.into_inner();
     println!("Incoming cmd is {}!", &cmd);
@@ -22,7 +22,7 @@ fn cmd(req: HttpRequest<AppState>) -> impl Responder {
     return format!("Incoming cmd is {}!", &curcmd);
 }
 
-fn power(req: HttpRequest<AppState>) -> impl Responder {
+fn on_power(req: HttpRequest<AppState>) -> impl Responder {
     let info = Path::<(usize)>::extract(&req).unwrap();
     let power = info.into_inner();
     println!("Incoming power is {}!", &power);
@@ -38,88 +38,90 @@ fn main() {
     let gpio = init_gpio();
     stop(&gpio);
 
-    let mutex = Arc::new(Mutex::new("".to_string()));
+    let cmd_mutex = Arc::new(Mutex::new("".to_string()));
     let power_atomic = Arc::new(AtomicUsize::new(100));
 
-    let clone = mutex.clone();
-    let power_clone = power_atomic.clone();
+    {
+        let cmd_mutex = cmd_mutex.clone();
+        let power_atomic = power_atomic.clone();
 
-    thread::spawn(move || {
-        let mut cmd = "stop".to_string();
-        let mut lastcmd = Instant::now();
-        let mut cpower: usize;
-        loop {
-            {
-                let mut mutex = clone.lock().unwrap();
-                if *mutex != "" {
-                    cmd = mutex.to_string();
-                    *mutex = "".to_string();
-                    lastcmd = Instant::now();
+        thread::spawn(move || {
+            let mut cmd = "stop".to_string();
+            let mut lastcmd = Instant::now();
+            let mut cpower: usize;
+            loop {
+                {
+                    let mut cmd_mutex = cmd_mutex.lock().unwrap();
+                    if *cmd_mutex != "" {
+                        cmd = cmd_mutex.to_string();
+                        *cmd_mutex = "".to_string();
+                        lastcmd = Instant::now();
 
-                    println!("new cmd: {}", cmd);
+                        println!("new cmd: {}", cmd);
+                    }
+
+                    cpower = power_atomic.load(Ordering::Relaxed);
                 }
 
-                cpower = power_clone.load(Ordering::Relaxed);
-            }
+                if lastcmd.elapsed().subsec_millis() > 500 {
+                    cmd = "stop".to_string();
+                }
 
-            if lastcmd.elapsed().subsec_millis() > 500 {
-                cmd = "stop".to_string();
-            }
+                let mut job: Option<Box<Fn(&Gpio)>> = None;
+                //let mut job = Fn(&Gpio);
 
-            let mut job: Option<Box<Fn(&Gpio)>> = None;
-            //let mut job = Fn(&Gpio);
+                if cmd == "stop" {
+                    job = Some(Box::new(|gp| stop(&gp)));
+                }
 
-            if cmd == "stop" {
-                job = Some(Box::new(|gp| stop(&gp)));
-            }
+                if cmd == "forward" {
+                    job = Some(Box::new(|gp| {
+                        r_f(&gp);
+                        l_f(&gp);
+                    }));
+                }
 
-            if cmd == "forward" {
-                job = Some(Box::new(|gp| {
-                    r_f(&gp);
-                    l_f(&gp);
-                }));
-            }
+                if cmd == "backward" {
+                    job = Some(Box::new(|gp| {
+                        r_b(&gp);
+                        l_b(&gp);
+                    }));
+                }
 
-            if cmd == "backward" {
-                job = Some(Box::new(|gp| {
-                    r_b(&gp);
-                    l_b(&gp);
-                }));
-            }
+                if cmd == "left" {
+                    job = Some(Box::new(|gp| {
+                        r_b(&gp);
+                        l_f(&gp);
+                    }));
+                }
 
-            if cmd == "left" {
-                job = Some(Box::new(|gp| {
-                    r_b(&gp);
-                    l_f(&gp);
-                }));
-            }
+                if cmd == "right" {
+                    job = Some(Box::new(|gp| {
+                        r_f(&gp);
+                        l_b(&gp);
+                    }));
+                }
 
-            if cmd == "right" {
-                job = Some(Box::new(|gp| {
-                    r_f(&gp);
-                    l_b(&gp);
-                }));
-            }
-
-            if let Some(job) = job {
-                if cpower >= 100 || cmd == "stop" {
-                    job(&gpio);
-                    thread::sleep(Duration::from_millis(10));
-                } else {
-                    pwm(&gpio, 10, cpower, job);
+                if let Some(job) = job {
+                    if cpower >= 100 || cmd == "stop" {
+                        job(&gpio);
+                        thread::sleep(Duration::from_millis(10));
+                    } else {
+                        pwm(&gpio, 10, cpower, job);
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 
     server::new(move || {
-        let mutex = mutex.clone();
-        let power_clone = power_atomic.clone();
+        let cmd_mutex = cmd_mutex.clone();
+        let power_atomic = power_atomic.clone();
         return App::with_state(AppState {
-            cmd: mutex,
-            power: power_clone,
-        }).route("/cmd/power/{power}", http::Method::GET, power)
-            .route("/cmd/{cmd}", http::Method::GET, cmd)
+            cmd: cmd_mutex,
+            power: power_atomic,
+        }).route("/cmd/power/{power}", http::Method::GET, on_power)
+            .route("/cmd/{cmd}", http::Method::GET, on_cmd)
             .handler(
                 "/",
                 fs::StaticFiles::new("./static/").index_file("index.html"),
@@ -180,11 +182,7 @@ fn pwm(gpio: &Gpio, millis: u32, prc: usize, f: Box<Fn(&Gpio)>) {
     );
 
     let start = Instant::now();
-    loop {
-        if start.elapsed().subsec_millis() > millis {
-            break;
-        }
-
+    while start.elapsed().subsec_millis() < millis {
         f(&gpio);
         thread::sleep(Duration::from_micros(wcycle as u64));
         stop(&gpio);
